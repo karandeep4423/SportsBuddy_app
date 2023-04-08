a736de3374238978c1faac0d3646a6c0865fb2e6
Karandeep Singh <karandeep>
2023-04-08 22:49:24 +0530

    api url

--- client/src/Store/api.js
+++ client/src/Store/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:8080" });
+const API = axios.create({ baseURL: "https://sportsbuddy.onrender.com/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("user")) {
