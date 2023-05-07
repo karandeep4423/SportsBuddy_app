fd50865a8d6cdc043b554355c6ecdfdd0ee64173
Karandeep Singh <karandeep>
2023-05-07 12:46:59 +0530

    added hashrouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App'
 import { Provider } from 'react-redux';
 import { Store } from './Store/Store'
 import './index.css'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HashRouter>
     <Provider store={Store}>
     <App  />
     </Provider>
+    </HashRouter>
   </React.StrictMode>
 )
